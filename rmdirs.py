7094fb7094381b81473a64ceb8d7196938f39e54
lbud <lbudorick@gmail.com>
2014-02-07 17:57:04 -0800

    Delete rmdirs.py

--- rmdirs.py
+++
@@ -1,8 +0,0 @@
-import os, shutil, glob
-
-foldernames = range(97, 123)
-foldernames[:] = [chr(foldername) for foldername in foldernames]
-
-[shutil.move(filename, 'files/') for foldername in foldernames for filename in glob.glob(foldername + '/*.txt')]
-
-[os.rmdir(eachfolder) for eachfolder in foldernames]
