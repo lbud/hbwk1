73762a8aeb8abc8701bdaf4f827ddc73da7b320c
Lauren Budorick <Lauren@lauren-budoricks-macbook-pro.local>
2014-02-14 12:18:55 -0800

    changed week1project.py because list comprehensions are unnecessary + added comments. didn't modify reset.py becaaaaause I'm stubborn and want to look back at my first-ever list comprehensions later

--- week1project.py
+++ week1project.py
@@ -1,17 +1,12 @@
 import shutil, os
 
+# create list of all sourec files
 source = os.listdir("files/")
 
+# make 26 folders, one for each letter of the alphabet using ascii vals
+for i in range(97, 123):
+    os.mkdir(chr(i))
 
-
-
-foldernames = range(97, 123)
-
-foldernames[:] = [chr(foldername) for foldername in foldernames]
-
-[os.mkdir(foldername) for foldername in foldernames]
-
-
-
-
-[shutil.move("files/" + filename, filename[0] + '/') for filename in source]
+# loop through filename list, move it to appropriate folder
+for filename in source:
+    shutil.move("files/" + filename, filename[0] + '/')
